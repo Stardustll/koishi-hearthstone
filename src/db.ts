1465fc50fcff8b0ab9b4c02a7000054452f954b0
Tflystar <tflystar@gmail.com>
2026-03-14 18:12:17 +0800

    feat: 添加卡牌查询功能，支持通过ID和名称查询卡牌信息

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,23 @@
 import Database from 'better-sqlite3';
 import path from 'path';
+import { json } from 'stream/consumers';
 
 const db = new Database(path.resolve(__dirname,'../data/hearthstone_cards.db'));
 
+export function searchCard_id(value){
+    const row = db.prepare(`SELECT image_normal_data from cards WHERE id = ?`).get(`%${value}%`);
+    return Buffer.from(row.image_normal_data).toString('base64');
+}
+
+export function searchCard_img(value){
+    const row = db.prepare(`SELECT image_normal_data from cards WHERE id = ?`).get(value);
+    return Buffer.from(row.image_normal_data).toString('base64');
+}
+
 export function searchCard(value){
-    const rows = db.prepare(`SELECT * from card_names WHERE name LIKE ?`).all(`%${value}%`);
-    const row = db.prepare(`SELECT image_normal_data from cards WHERE id = ?`).all(rows[0].id);
-    // return JSON.stringify(row);
-    // return rows.map(r => r.name).join('\n');
-    return Buffer.from(row[0].image_normal_data).toString('base64');
+    const row = db.prepare(`SELECT a.id, a.name, b.tag_value from card_names a JOIN card_tags b ON a.id = b.card_id WHERE a.name LIKE ? AND b.tag_id = 321`).all(`%${value}%`);
+    // const result = '卡牌ID		卡牌名			可否收藏\n' + 
+	// 				row.map(r => `${String(r.id).padEnd(7, ' ')}		${r.name.padEnd(15, ' ')}     ${r.tag_value === 1 ? '是' : '否'}`).join('\n');
+    const result = row.map(r => `ID: ${r.id}\n名称: ${r.name}\n可收藏: ${r.tag_value === 1 ? '是' : '否'}`).join('\n---\n')
+	return result;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Context, Schema } from 'koishi'
-import { searchCard } from './db'
+import { searchCard_id,searchCard_img,searchCard } from './db'
 import path from 'path'
 
 export const name = 'hearthstone'
@@ -10,16 +10,15 @@ export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context, config: Config) {
   // write your plugin here
-  // 如果收到“天王盖地虎”，就回应“宝塔镇河妖”
-  ctx.on('message', (session) => {
-    if (session.content === '测试') {
-      session.send(path.resolve(__dirname))
-    }
-  })
 
   ctx.command('卡牌查询 <message>')
     .action((_, message) => {
-      return `<image url="data:image/png;base64,${searchCard(message)}"/>`
-      
+      return searchCard(message);
+      // return `<image url="data:image/png;base64,${searchCard(message)}"/>`
+    })
+  
+  ctx.command('id查询卡牌 <message>').alias('id查询')
+    .action((_, message) => {
+      return `<image url="data:image/png;base64,${searchCard_img(message)}"/>`
     })
 }
